fd1b619c72969ac8398d4b0b0e625b6601d4ddf6
Eunhye0k <dmsgur7305@naver.com>
2025-08-26 11:06:44 +0900

    S3BinaryContentStorage 나머지 기능 구현, application.yml 수정

--- src/main/java/com/sprint/mission/discodeit/storage/s3/S3BinaryContentStorage.java
+++ src/main/java/com/sprint/mission/discodeit/storage/s3/S3BinaryContentStorage.java
@@ -2,16 +2,25 @@ package com.sprint.mission.discodeit.storage.s3;
 
 import com.sprint.mission.discodeit.dto.data.BinaryContentDto;
 import com.sprint.mission.discodeit.storage.BinaryContentStorage;
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.time.Duration;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
 
 @Component
 @RequiredArgsConstructor
@@ -49,19 +58,45 @@ public class S3BinaryContentStorage implements BinaryContentStorage {
 
   @Override
   public InputStream get(UUID binaryContentId) {
-    return null;
+    S3Client s3 = getS3Client();
+    GetObjectResponse response = s3.getObject(GetObjectRequest.builder()
+        .bucket(bucket)
+        .key(binaryContentId.toString())
+        .build()).response();
+    return new ByteArrayInputStream(response.toString().getBytes());
   }
 
   @Override
   public ResponseEntity<Void> download(BinaryContentDto metaData) {
-    return null;
+    String url = generatePresignedUrl(metaData.id().toString(), metaData.contentType());
+    return ResponseEntity.status(302).header("Location", url).build();
   }
 
   public S3Client getS3Client() {
-    return null;
+    return S3Client.builder()
+        .region(Region.of(region))
+        .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+        .build();
   }
 
   public String generatePresignedUrl(String key, String contentType) {
-    return null;
+    try(S3Presigner presigner = S3Presigner.builder()
+        .region(Region.of(region))
+        .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+        .build()) {
+
+      GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+          .bucket(bucket)
+          .key(key)
+          .responseContentType(contentType)
+          .build();
+
+      GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
+          .signatureDuration(Duration.ofHours(10))
+          .getObjectRequest(getObjectRequest)
+          .build();
+
+    return presigner.presignGetObject(presignRequest).url().toString();
+    }
   }
 }
